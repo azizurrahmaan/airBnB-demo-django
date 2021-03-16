db568bbeac55492795b7dfec529945a8b29de03f
fanuman <numanfarukh22@gmail.com>
2021-03-16 20:30:47 +0500

    change deployment script

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,6 @@ echo "Install requirements..."
 . /var/www/airbnb_project/venv/bin/activate
 
 pip install -r /var/www/airbnb_project/requirements.txt
-chown -R www-data:www-data /var/www/airbnb_project/
 
 echo "Main service reload..."
 sudo systemctl restart nginx
